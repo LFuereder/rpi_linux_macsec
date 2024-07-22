302c2075fbb6c3075ba521b7285fed8f651e3463
Lukas Fuereder <lukas.fuereder@oth-regensburg.de>
2024-07-22 10:31:33 +0200

    adapted initial version of the pci_driver test kernel module.

--- linux_drvs/driver-test.c
+++ linux_drvs/driver-test.c
@@ -3,9 +3,11 @@
 
 #define PCI_TEST_VENDOR_ID 0x10ee
 #define PCI_TEST_DEVICE_ID 0x7021
-#define PCI_TEST_DEVICE_MEMOFFSET 0x0
+#define PCI_TEST_DEVICE_MEMOFFSET 0x0000
 
-void __iomem *ptr_mem0;
+void __iomem *ptr_mem;
+
+int mem_id = 1;
 
 /* Meta Inforamtion */
 MODULE_LICENSE("GPL");
@@ -25,17 +27,56 @@ MODULE_DEVICE_TABLE(pci, pci_test_driver_ids);
 static void gather_device_info(struct pci_dev *dev)
 {
     /* Gather information about memory space on device */
-    int status = pci_resource_len(dev, 0);
+    int status = pci_resource_len(dev, mem_id);
     printk("---------------------------------------------------\n");
-    printk("[ INFO ]   pci_test_driver BAR0 is %d bytes\n", status);
-    printk("[ INFO ]   pci_test_driver BAR0 is mapped to 0x%llx\n", pci_resource_start(dev, 0));
+    printk("[ INFO ]   pci_test_driver MEM is %d bytes\n", status);
+    printk("[ INFO ]   pci_test_driver MEM is mapped to 0x%llx\n", pci_resource_start(dev, mem_id));
+    printk("[ INFO ]   pci_test_driver MEM Offset: 0x%X\n", PCI_TEST_DEVICE_MEMOFFSET);
     printk("---------------------------------------------------\n");
+}
+
+static int access_pci_mem(struct pci_dev *dev)
+{
+    ptr_mem = NULL;
+
+    int status = pcim_iomap_regions(dev, BIT(mem_id), KBUILD_MODNAME);
+    if( status < 0)
+    {
+        printk("[ ERROR ]   Could not enable pci mem_%d\n", mem_id);
+        return status;
+    }
+
+    printk("[ INFO ]   Successfully enabled pci mem_%d\n", mem_id);
+
+
+    ptr_mem = pcim_iomap_table(dev)[mem_id];
+    if(ptr_mem == NULL)
+    {
+        printk("[ ERROR ]   Invalid pointer for mem_%d\n", mem_id);
+        return -1;
+    }
+
+    printk("[ INFO ]   Value of ptr_mem0: 0x%p\n", ptr_mem);
+
+    printk("[ TEST ]   pci mem_%d original value: 0x%x\n", mem_id, ioread32(ptr_mem + PCI_TEST_DEVICE_MEMOFFSET));
+
+    if(PCI_TEST_DEVICE_MEMOFFSET == 0)
+    {
+        iowrite8(0xB, ptr_mem + PCI_TEST_DEVICE_MEMOFFSET);
+    } else if(PCI_TEST_DEVICE_MEMOFFSET == 0x4000)
+    {
+        iowrite8(0xC, ptr_mem + PCI_TEST_DEVICE_MEMOFFSET);
+    }
+
+
+    printk("[ TEST ]   pci mem_%d new value: 0x%x\n", mem_id, ioread32(ptr_mem + PCI_TEST_DEVICE_MEMOFFSET));
+
+    return 0;
 }
 
 /* The following function is called, if a device is registered */
 static int pci_test_device_probe(struct pci_dev *dev, const struct pci_device_id *id)
 {
-    ptr_mem0 = NULL;
     int status;
 
     printk("[ INFO ]   pci_test_driver currently in the probe step\n");
@@ -48,29 +89,9 @@ static int pci_test_device_probe(struct pci_dev *dev, const struct pci_device_id
         printk("[ ERROR ]   Could not enable pci_test_driver\n");
         return status;
     }
-
-    status = pcim_iomap_regions(dev, BIT(1), KBUILD_MODNAME);
-    if( status < 0)
-    {
-        printk("[ ERROR ]   Could not enable pci_test_driver\n");
-        return status;
-    }
-
-    printk("[ INFO ]   Successfully enabled pci_test_driver\n");
-
-
-    ptr_mem0 = pcim_iomap_table(dev)[1];
-    if(ptr_mem0 == NULL)
-    {
-        printk("[ ERROR ]   Invalid pointer for BAR0\n");
-        return -1;
-    }
-
-    printk("[ INFO ]   Value of ptr_mem0: 0x%p\n", ptr_mem0);
-
-    printk("pci_test_driver mem0 state DWord: 0x%x\n", ioread32(ptr_mem0 + PCI_TEST_DEVICE_MEMOFFSET));
-    iowrite8(0xb, ptr_mem0 + PCI_TEST_DEVICE_MEMOFFSET);
     
+    access_pci_mem(dev);
+
     return 0;
 }
 
@@ -79,9 +100,8 @@ static int pci_test_device_probe(struct pci_dev *dev, const struct pci_device_id
 static void pci_test_device_remove(struct pci_dev *dev)
 {
     printk("[ INFO ]   pci_test_driver currently in the remove step\n");
-
-    printk("pci_test_driver mem0 state DWord: 0x%x\n", ioread32(ptr_mem0 + PCI_TEST_DEVICE_MEMOFFSET));
-    iowrite8(0x0, ptr_mem0 + PCI_TEST_DEVICE_MEMOFFSET);
+    iowrite8(0x0, ptr_mem + PCI_TEST_DEVICE_MEMOFFSET);
+    printk("=======================================================\n");
     printk("=======================================================\n");
     return;
 }
