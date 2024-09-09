54a8841d605ba801a4eeea3f8e146a5727682b4f
Lukas Fuereder <lukas.fuereder@oth-regensburg.de>
2024-09-09 09:36:48 +0200

    integraded initial environment for in-kernel dma-to-pcie access.

---
+++ linux_drvs/driver_test/dma_kernel_drv_access/driver-test.c
@@ -0,0 +1,30 @@
+#include "driver-test.h"
+#include "gpio_driver.h"
+
+/* Meta Information */
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Lukas Fuereder");
+MODULE_DESCRIPTION("Generic Test Driver, if probed accesses the gpio_driver and modifies its current entry.");
+
+/**
+ * @brief This function is called, when the module is loaded into the kernel
+ */
+static int __init my_init(void)
+{
+	printk("file_access - Loading driver\n");
+	driver_toggle_gpio();
+	
+	return 0;
+}
+
+/**
+ * @brief This function is called, when the module is removed from the kernel
+ */
+static void __exit my_exit(void)
+{
+	printk("file_access - Unloading driver\n");
+	driver_toggle_gpio();
+}
+
+module_init(my_init);
+module_exit(my_exit);
