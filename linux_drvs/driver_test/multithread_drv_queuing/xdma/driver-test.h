6a42caeaf8be4467bba0e1037765ce199a26acf8
Lukas Fuereder <lukas.fuereder@oth-regensburg.de>
2024-09-18 10:13:49 +0200

    added new folder for queuing implementation in the driver-test folder of the repository.

---
+++ linux_drvs/driver_test/multithread_drv_queuing/xdma/driver-test.h
@@ -0,0 +1,16 @@
+#ifndef DRV_TST_H
+#define DRV_TST_H
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/file.h>
+#include <linux/export.h>
+
+/* cross-driver API */
+
+extern ssize_t drv_access_char_sgdma_write(const char *buf, size_t count, loff_t *pos);
+extern ssize_t drv_access_char_sgdma_read(const char *buf, size_t count, loff_t *pos);
+
+#endif
