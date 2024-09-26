cf7c0cf5d168d3bc12201b04c6207f0e968e1808
Lukas Fuereder <lukas.fuereder@oth-regensburg.de>
2024-09-26 08:19:24 +0200

    implemented waitqueue functionality to reduce resource consumption.

--- linux_drvs/driver_test/multithread_drv_queuing/xdma/kritis3m_egress_kthread.h
+++ linux_drvs/driver_test/multithread_drv_queuing/xdma/kritis3m_egress_kthread.h
@@ -6,12 +6,14 @@
 #include <linux/fs.h>
 #include <linux/spinlock.h>
 #include <linux/uaccess.h>
+#include <linux/jiffies.h>
 #include <linux/file.h>
 #include <linux/export.h>
 #include <linux/sched.h>
 #include <linux/kthread.h>
 #include <linux/delay.h>
 #include <linux/string.h>
+#include <linux/wait.h>
 
 #include "xdma_mod.h"
 
@@ -33,6 +35,10 @@ struct kritis3m_egress_queue
 
     /*  thread work list count */
 	unsigned int work_cnt;
+
+    /* thread wait queue parameter */
+	wait_queue_head_t waitq;
+
 };
 
 struct kritis3m_queue_element
@@ -45,10 +51,10 @@ struct kritis3m_queue_element
 };
 
 
-/* API to the egress queue */
+/* Input API to the egress queue */
 ssize_t egress_thread_add_work(const char *buf, size_t count);
 
-/* XDMA cross-driver API */
+/* Output XDMA cross-driver API */
 extern ssize_t drv_access_char_sgdma_write(struct xdma_cdev *xcdev, const char *buf, size_t count, loff_t *pos);
 
 #endif
